13065cfbd6ab4cc0c8ad345bd790927245f79f10
marcelolemes <lemmarcelo@gmail.com>
2015-05-20 10:12:37 -0300

    Primeiro Commit

---
+++ Conferir Pasta Bustos/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Conferir_Pasta_Bustos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(96, 55);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 90);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 128);
+            this.MinimumSize = new System.Drawing.Size(300, 128);
+            this.Name = "Form1";
+            this.Text = "Conferir Pasta Bustos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ Conferir Pasta Bustos/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Conferir_Pasta_Bustos
+{
+    public partial class Form1 : Form
+    {
+        List<String> pastadeAlbuns = new List<String>();
+        //   List<String> pastadeBustos = new List<String>();
+        List<String> albunsSemBusto = new List<String>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+
+
+            conferirPastas();
+
+
+        }
+
+
+        private void conferirPastas()
+        {
+            pastadeAlbuns.Clear();
+            albunsSemBusto.Clear();
+            if (Directory.Exists(textBox1.Text))
+            {  // Verifcando se o caminho inserido é válido
+
+                pastadeAlbuns.AddRange(Directory.GetDirectories(textBox1.Text, "20*"));
+                foreach (String s in pastadeAlbuns)
+                {
+
+                    String[] parcial = s.Split('\\'); // Obtendo o nome da foto, extraindo com caminho geral do arquivo
+                    String nomeAlbum = parcial[parcial.Length - 1];
+                    if (Directory.Exists(textBox1.Text + "/Bustos Tratados/" + nomeAlbum))
+                    {
+
+                    }
+                    else
+                    {
+                        albunsSemBusto.Add(nomeAlbum);
+
+                    }
+                }
+
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(textBox1.Text + "//Conferência da pasta bustos.txt"))
+                {
+                    foreach (String i in albunsSemBusto)
+                    {
+                        file.WriteLine(i + " não tem pasta bustos!");
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um caminho válido");
+            }
+
+            MessageBox.Show("Pronto!");
+        }
+
+    }
+}
